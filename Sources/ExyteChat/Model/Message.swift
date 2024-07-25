f08dc63afdd892e8ee646b480326d04f9556351d
walterh <waltermwfs@gmail.com>
2024-07-25 10:46:48 -0400

    Make == support .text

--- Sources/ExyteChat/Model/Message.swift
+++ Sources/ExyteChat/Model/Message.swift
@@ -108,7 +108,7 @@ extension Message {
 
 extension Message: Equatable {
     public static func == (lhs: Message, rhs: Message) -> Bool {
-        lhs.id == rhs.id && lhs.status == rhs.status
+        lhs.id == rhs.id && lhs.status == rhs.status && lhs.text == rhs.text
     }
 }
 
